b307c98478016cde376e947dcbdf4db85cffa823
zanotelli <zanotellisimon@gmail.com>
2021-01-29 13:16:33 +0100

    création fichiers de base html, vue et routers

---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- App content -->
+    <about-us></about-us>
+    <!-- App footer -->
+    <layout></layout>
+  </div>
+</template>
+
+<script>
+import AboutUs from '@/components/views/AboutUs.vue';
+import Layout from '@/components/Layout.vue';
+
+export default {
+  name: 'About',
+  components: {
+        AboutUs,
+        Layout,
+        },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
+// Fontawesome Icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHeart, faCopyright } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-Vue.config.productionTip = false
+import App from './App.vue'
+import router from './router'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
+
+library.add(faHeart, faCopyright)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
