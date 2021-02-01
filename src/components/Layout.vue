e2379616e522f41710772730a5366235034c40c8
aoumar <aoumar.lahouari@gmail.com>
2021-02-01 10:52:50 +0100

    Update logo

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,22 +1,18 @@
 <template>
-  <v-footer class="secondary" app>
-      <v-layout row wrap align-center>
-        <v-flex xs12>
-          <div>
-            <v-card-title>
-            <img src="../images/logo1.png" alt="logo app" style="height: 100px;">
-            <strong class="subheading">Get connected with us on social networks!</strong>
-            <v-spacer></v-spacer>
-            <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
-            <v-icon size="26px">{{ icon }}</v-icon>
-            </v-btn>
-            <v-card-text class=" white--text text-center">{{ new Date().getFullYear() }} — <strong>VUEMUSIC</strong>
-            </v-card-text>
-            
-            </v-card-title>
-          </div>
-        </v-flex>
-      </v-layout>
+  <v-footer dark fixed>
+    <v-card class="flex" flat tile >
+      <v-card-title class="dark">
+<img src="../images/logo.png" alt="logo app" style="height: 100px;">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+        <v-spacer></v-spacer>
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-card-text class="py-2 white--text text-center">
+        Copyright @ {{ new Date().getFullYear() }} — <strong>Vue Music</strong>
+      </v-card-text>
+    </v-card>
   </v-footer>
 </template>
 <script>
