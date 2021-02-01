e9bc61a92a4166453d626edd3f439032a393c8f9
zanotelli <zanotellisimon@gmail.com>
2021-02-01 16:53:15 +0100

    modification hottracks

--- src/components/HotTracks.vue
+++ src/components/HotTracks.vue
@@ -1,40 +1,42 @@
 <template>
 <div class="container">
-    
     <div class="row bg-dark">
-        <div class="col-md-3 text-white">#</div>
-        <div class="col-md-3 text-white">Nom</div>
-        <div class="col-md-3 text-white">eId</div>
-        <div class="col-md-3 text-white">Image</div>
+        <div class="col-md-3 text-white">Name</div>
+        <div class="col-md-3 text-white">image</div>
+        <div class="col-md-3 text-white">track</div>
     </div>
-    <div v-for="track in tracks" :key="track._id">
-        <div class="col-md-3 text-white border">{{track._id}}</div>
-        <div class="col-md-3 text-white border"><h5>{{track.name}}</h5></div>
-        <div class="col-md-3 text-white border">{{track.eId}}</div>
+   <div v-for="track in tracks" :key="track._id" class="row bg-secondary">
+        <div class="col-md-3 text-white border">{{track.name}}</div>
         <div class="col-md-3 text-white border">{{track.img}}</div>
-    </div>
-    
+        <div class="col-md-3 text-white border">{{track.track}}</div>
+   </div>
+      
 </div>
 </template>
 <script>
 import axios from 'axios';
 
 export default {
-  
-  name: 'HotTracks',
-  data: () => ({
-    tracks: [],
-    errors: []
-  }),
-  created () {
-    axios.get('https://openwhyd.org/hot?format=json')
-      .then(response => {
-        this.tracks = response.data.DISPLAY
-        console.log(response)
-      })
-      .catch(e => {
-        this.errors.push(e)
-      })
+  name: "hottrackslist",
+  data() {
+    return {
+      tracks: [],
+    };
+  },
+beforeMount() {
+    this.getHotTracks();
+  },
+methods: {
+  getTracks() {
+            return axios.get('https://openwhyd.org/hot');
+      },
+    async getHotTracks() {
+      const response = await this.getTracks();
+      this.tracks = response.data.results;
+    },
+    
   }
-}
+};
+
+
 </script>
