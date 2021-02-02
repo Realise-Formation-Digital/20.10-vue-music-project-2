db34fccaf899116de9901513726d1a40267fdc3c
Boo <aurelieboo1219@gmail.com>
2021-02-02 12:05:58 +0100

    carousel

--- src/components/views/Carousel.vue
+++ src/components/views/Carousel.vue
@@ -1,34 +1,36 @@
 <template>
-  <v-carousel hide-delimiters style="height:300px;">
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src="item.src"
-      reverse-transition="fade-transition"
-      transition="fade-transition"
-    ></v-carousel-item>
-  </v-carousel>
+  <div>
+    <v-carousel cycle hide-delimiters style="height:300px;">
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      ></v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 <script>
-  export default {
-    name:"Carousel",
-    data () {
-      return {
-        items: [
-          {
-            src: require('../../images/carousel.jpg'),
-          },
-          {
-            src: require('../../images/carousel1.jpg'),
-          },
-          {
-            src: require('../../images/carousel2.jpg'),
-          },
-          {  
-            src: require('../../images/carousel3.jpeg'),
-          },  
-        ],
-      }
-    },
-  }
+export default {
+  name: "Carousel",
+  data() {
+    return {
+      items: [
+        {
+          src: require("../../images/carousel.jpg"),
+        },
+        {
+          src: require("../../images/carousel1.jpg"),
+        },
+        {
+          src: require("../../images/carousel2.jpg"),
+        },
+        {
+          src: require("../../images/carousel3.jpeg"),
+        },
+      ],
+    };
+  },
+};
 </script>
