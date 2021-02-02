cd423ff9343342eebe1a6a05a77cc80d501154d3
zanotelli <zanotellisimon@gmail.com>
2021-02-02 17:44:28 +0100

    Update temporaire HotTracks component

--- src/components/HotTracks.vue
+++ src/components/HotTracks.vue
@@ -1,17 +1,15 @@
 <template>
-<div class="container">
-    <div class="row bg-dark">
-        <div class="col-md-3 text-white">Name</div>
-        <div class="col-md-3 text-white">image</div>
-        <div class="col-md-3 text-white">Posted</div>
+<div class="container-fluid py-5 px-10" style="background-color:crimson;">
+    <div v-for="track in tracks" :key="track._id" class="row">
+        <div  v-for="track in tracks" :key="track._id" class="col-md-4">
+            <div style="background-color:white;" class="thumbnail-img shadow p-3 mb-5 bg-white rounded">
+                <img :src="track.img" width="250" height="200" classe="shadow-2-strong" />
+                <p><a :href="`https://youtube.com/watch?v=${track.eId}`">{{track.name}}</a></p>
+            </div>
+        </div>
     </div>
-   <div v-for="track in tracks" :key="track._id" class="row bg-secondary">
-        <div class="col-md-3 text-white border">{{track.name}}</div>
-        <div class="col-md-3 text-white border">{{track.img}}</div>
-        <div class="col-md-3 text-white border">{{track.uNm}}</div>
-   </div>
-      
-</div>
+  </div> 
+ 
 </template>
 <script>
 import axios from 'axios';
@@ -19,7 +17,7 @@ import axios from 'axios';
 export default {
   name: "hottrackslist",
   data() {
-    return {
+      return {
       tracks: [],
     };
   },
@@ -29,7 +27,7 @@ beforeMount() {
 methods: {
     async getTracks() {
       try{
-        const result = await axios.get('https://cors-anywhere.herokuapp.com/https://openwhyd.org/hot/electro?format=json');
+        const result = await axios.get('https://cors-anywhere.herokuapp.com/https://openwhyd.org/hot?format=json');
         console.log("data Fetched", result)
         //this.tracks = result.data.tracks
         return result.data.tracks
@@ -42,8 +40,27 @@ methods: {
       this.tracks = response;
     },
     
+
+    
   }
+
 };
 
 
 </script>
+<style scoped>
+  .thumbnail{
+    display:flex;
+  }
+  .thumbnail img{
+    width:250px;
+}
+
+.thumbnail-info{
+    margin-left:20px;
+}
+
+.thumbnail h3{
+    font-size:16px;
+}
+</style>
